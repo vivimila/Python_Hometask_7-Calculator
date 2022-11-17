74ca13ce9acb37ad6f966113bf4bf1c2eeeaf6f1
vivimila <violamilashevskaya@yandex.by>
2022-11-17 18:36:14 +0300

    calculator is solved

--- Calculator.py
+++ Calculator.py
@@ -1,16 +1,29 @@
-#Создать калькулятор.
+# Создать калькулятор.
 
-number_1 = int(input('Enter your first number: '))
-number_2 = int(input('Enter your second number: '))
-# Addition
-print('{} + {} = '.format(number_1, number_2))
-print(number_1 + number_2)
-# Subtraction
-print('{} - {} = '.format(number_1, number_2))
-print(number_1 - number_2)
-# Multiplication
-print('{} * {} = '.format(number_1, number_2))
-print(number_1 * number_2)
-# Division
-print('{} / {} = '.format(number_1, number_2))
-print(number_1 / number_2)
+#greeting = int(input(f'Калькулятор приветствует тебя!)'))
+
+number_1 = int(input('Введите первое значение: '))
+
+operation = input(f'Выберите действие: +, -, *, /   ')
+#+ for addition
+#- for subtraction
+#* for multiplication
+#/ for division
+
+number_2 = int(input('Введите второе значение: '))
+
+if operation == '+':
+    print('{} + {} = '.format(number_1, number_2))
+    print(number_1 + number_2)
+elif operation == '-':
+    print('{} - {} = '.format(number_1, number_2))
+    print(number_1 - number_2)
+elif operation == '*':
+    print('{} * {} = '.format(number_1, number_2))
+    print(number_1 * number_2)
+elif operation == '/':
+    print('{} / {} = '.format(number_1, number_2))
+    print(number_1 / number_2)
+else:
+    print('Вы ввели неверный оператор действия. Повторите попытку.')
+
