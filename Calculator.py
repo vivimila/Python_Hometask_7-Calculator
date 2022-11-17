448ef574c7481b853c2606dccb48413802d64144
vivimila <violamilashevskaya@yandex.by>
2022-11-17 17:54:38 +0300

    add structure

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,16 @@
 #Создать калькулятор.
+
+number_1 = int(input('Enter your first number: '))
+number_2 = int(input('Enter your second number: '))
+# Addition
+print('{} + {} = '.format(number_1, number_2))
+print(number_1 + number_2)
+# Subtraction
+print('{} - {} = '.format(number_1, number_2))
+print(number_1 - number_2)
+# Multiplication
+print('{} * {} = '.format(number_1, number_2))
+print(number_1 * number_2)
+# Division
+print('{} / {} = '.format(number_1, number_2))
+print(number_1 / number_2)
